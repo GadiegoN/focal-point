fbba32d194c374560713c4764794aa79590eacd4
GadiegoN <ngadiego@gmail.com>
2024-09-19 17:08:46 -0300

    feat: create header component

---
+++ src/components/header/index.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Logo from "@/assets/images/logo.png";
+import styles from "./header.module.scss";
+import { Heading } from "../headding";
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.headerContainer}>
+        <Image src={Logo} alt="Logo focal point" width={150} height={36} />
+
+        <Heading varaint="title">Bem vindo de volta, Gadiego</Heading>
+
+        <Heading varaint="subtitle">Segunda, 01 de dezembro de 2025</Heading>
+      </div>
+    </header>
+  );
+}
